7eb73e5fb5329942e6fcfbe41e470d5abf928cd3
Carlo Occhiena <57464184+carloocchiena@users.noreply.github.com>
2022-04-14 22:38:35 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,9 +1,12 @@
 from bs4 import BeautifulSoup as bs4
 import requests
 
+header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36'}
+
 def scraper(url):
-    page = requests.get(url).content
+    page = requests.get(url, headers=header).content
     soup = bs4(page, "html.parser").findAll("p")
     for div in soup:
         with open("raw_text.txt", mode="a", encoding="utf-8",) as p:
             p.write(div.text)
+            
