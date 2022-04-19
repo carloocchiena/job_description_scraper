95349a8010c8348dc927187d08c5bda188ec78bf
Carlo Occhiena <57464184+carloocchiena@users.noreply.github.com>
2022-04-19 12:22:37 +0200

    Update stopwords.py

--- stopwords.py
+++ stopwords.py
@@ -1,13 +1,12 @@
 import nltk
 from nltk.corpus import stopwords
-
 nltk.download('stopwords')
 nltk.download('punkt')
 
 
 # create our custom stopwords list
-symbols = ["!","'","£","$","%","&","(",")","?","^","*","+","/","-", "©", "·", "•", "|", "-", "–"]
+symbols = ["!", "'", "£", "$", "%", "&", "(",")", "?", "^", "*", "+", "/", "-", "©", "·", "•", "|", "-", "–", ',', ";"]
 
-cookie = ["cookie", "navigazione", "sito", "consenso", "navigazione", "accetta", "rifiuta", "gestisci", "utilizziamo", "funzionali", "tecnici", "informativa", "preferenze"]
+cookie = ["cookie", "cookies", "navigazione", "sito", "consenso", "accetta", "rifiuta", "gestisci", "utilizziamo", "funzionali", "tecnici", "informativa", "preferenze"]
 
 stopwords_overall = stopwords.words('italian')  + stopwords.words('english') + symbols + cookie
